5665a3223c0c92c97b1e8bffcb3d87564abd1d3b
nameisalfio <alfio.spoto@outlook.it>
2023-12-09 13:52:55 +0100

    students

---
+++ Itinere_2/Servlet/Students/src/main/java/com/StudentServlet.java
@@ -0,0 +1,159 @@
+package com;
+
+import java.io.*;
+import java.sql.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/servlet")
+public class StudentServlet extends HttpServlet{
+    
+    Connection conn = null;
+
+    public void init() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/University?user=root&password=root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+
+            String corso_ = (String)req.getParameter("corso");
+            String nome = (String)req.getParameter("nome");
+            String matricola = (String)req.getParameter("matricola");
+
+            String query = "SELECT * FROM courses WHERE codice_corso=?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, corso_);
+            ResultSet res = stmt.executeQuery();
+
+            if (res.next()) {
+                out.print("<h2>Studente "+nome+" :</h2>");
+                do {
+                    out.print("<p>Codice corso: "+res.getString("codice_corso")+
+                              ", descrizione: "+res.getString("descrizione")+
+                              ", crediti: "+res.getString("crediti")+"</p>");
+                } while (res.next());
+            } else {
+                out.print("Nessun corso di laurea assocciato allo studente "+nome+"");
+            }
+
+            String corso = getNomeCorso(corso_);
+
+            out.print("<br><h3>Cambia corso di laurea</h3>"+
+                        "<form action='/servlet' method='POST'>"+
+                            "<label for='corso'>Corso di laurea: </label>"+
+                            "<input type='text' id='corso' name='corso'><br><br>"+
+                            "<button type='submit'>Aggiorna corso</button>"+
+                            "<input type='hidden' name='action' value='update'>"+
+                            "<input type='hidden' name='matricola' value='"+matricola+"'>"+
+                            "<input type='hidden' name='corso' value='"+corso+"'>"+
+                       "</form><br>");
+
+            out.print("<br><h3>Rimuovi studente "+nome+"</h3>"+
+            "<form action='/servlet' method='POST'>"+
+                "<button type='submit'>Rimuovi studente</button>"+
+                "<input type='hidden' name='action' value='delete'>"+
+                "<input type='hidden' name='matricola' value='"+matricola+"'>"+
+            "</form><br>");
+
+            out.print("<a href='/index.jsp'><button>Torna alla home page</button></a>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+
+            if (req.getParameter("action") != null) {
+                
+                if (req.getParameter("action").equals("create")) {
+
+                    String nome = req.getParameter("nome");
+                    String cognome = req.getParameter("cognome");
+                    String corso = getIdCorso(req.getParameter("corso"));
+
+                    if (!corso.startsWith("Errore!")) {
+
+                        String query = "INSERT INTO students (nome, cognome, corso_di_laurea) VALUES (?,?,?)";
+                        PreparedStatement stmt = conn.prepareStatement(query);
+                        stmt.setString(1, nome);
+                        stmt.setString(2, cognome);
+                        stmt.setString(3, corso);
+    
+                        stmt.executeUpdate();
+                        out.print("<h3>Studente "+nome+" inserito con successo</h3>");
+
+                    } else {
+                        out.print("<h3>"+corso+"</h3>");
+                    }
+                }
+
+                if (req.getParameter("action").equals("update")) {
+                    String matricola = req.getParameter("matricola");
+                    String corso = getIdCorso(req.getParameter("corso"));
+
+                    String query = "UPDATE students SET corso_di_laurea=? WHERE matricola=?";
+                    PreparedStatement stmt = conn.prepareStatement(query);
+                    stmt.setString(1, corso);
+                    stmt.setString(2, matricola);
+
+                    stmt.executeUpdate();
+                    out.print("<h3>Corso di laurea aggiornato con successo</h3>");
+                }
+
+                if (req.getParameter("action").equals("delete")) {
+                    String matricola = req.getParameter("matricola");
+
+                    String query = "DELETE FROM students WHERE matricola = ?";
+                    PreparedStatement stmt = conn.prepareStatement(query);
+                    stmt.setString(1, matricola);
+
+                    stmt.executeUpdate();
+                    out.print("<h3>Studente rimosso con successo</h3>");
+                }
+            }
+            out.print("<a href='/index.jsp'><button>Torna alla home page</button></a>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    String getNomeCorso(String id) {
+        try {
+            String query_ = "SELECT nome_corso FROM courses WHERE codice_corso=?";
+            PreparedStatement stmt_ = conn.prepareStatement(query_);
+            stmt_.setString(1, id);
+            ResultSet res = stmt_.executeQuery();
+
+            if (res.next())
+                return res.getString("nome_corso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Errore! Corso di laurea non disponibile";
+    }
+
+    String getIdCorso(String nome) {
+        try {
+            String query_ = "SELECT codice_corso FROM courses WHERE nome_corso=?";
+            PreparedStatement stmt_ = conn.prepareStatement(query_);
+            stmt_.setString(1, nome);
+            ResultSet res = stmt_.executeQuery();
+
+            if (res.next())
+                return res.getString("codice_corso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Errore! Corso di laurea non disponibile";
+    }
+}
---
+++ Itinere_2/Servlet/Students/src/main/webapp/index.jsp
@@ -0,0 +1,82 @@
+<!--
+    CONSEGNA : 
+
+    Sviluppare una WebApp per la gestione degli studenti di un'università.
+
+    - Visualizzazione degli Studenti:
+
+    La homepage della WebApp deve mostrare la lista degli studenti presenti nel database.
+    Per ogni studente, devono essere visualizzati: matricola, nome, cognome e corso di laurea.
+    Se nessuno studente è presente, visualizzare il messaggio "Nessuno studente presente".
+
+    - Inserimento Studente:
+
+    Deve essere possibile inserire un nuovo studente tramite un modulo HTML.
+    Il modulo deve richiedere il nome, cognome e corso di laurea del nuovo studente.
+    Dopo l'inserimento, visualizzare un messaggio di conferma.
+
+    - Visualizzazione Dettagli Studente:
+
+    Cliccando sul corso di laurea di uno studente, la WebApp deve mostrare i dettagli del corso di laurea.
+    Se il corso di laurea non è associato a nessun corso nel database, visualizzare un messaggio appropriato.
+
+    (OPZIONALE)
+
+    Nella pagina che mostra i dettagli si necessita di :
+
+    - Aggiornamento Corso di Laurea:
+
+    La WebApp deve consentire di cambiare il corso di laurea di uno studente.
+    Utilizzare un form per raccogliere il nuovo corso di laurea.
+    Dopo l'aggiornamento, visualizzare un messaggio di conferma.
+    
+    - Rimozione Studente:
+
+    Permettere la rimozione di uno studente tramite un'apposita funzionalità.
+    Chiedere conferma prima di procedere con la rimozione.
+    Visualizzare un messaggio di conferma dopo la rimozione.
+-->
+
+<html>
+    <head>
+        <%@ page import="java.sql.*"%>
+        <%
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/University?user=root&password=root");
+
+            String query = "SELECT * FROM students";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet res = stmt.executeQuery();
+
+            if (res.next()) {
+                out.print("<h2>Studenti presenti:</h2>");
+                do {
+                    out.print("<p>Matricola: "+res.getString("matricola")+
+                        ", nome: "+res.getString("nome")+
+                        ", Cognome: "+res.getString("cognome")+
+                        ", Corso di laurea: <a href='/servlet?nome="+res.getString("nome")+
+                                                           "&corso="+res.getString("corso_di_laurea")+
+                                                           "&matricola="+res.getString("matricola")+"'>"+res.getString("corso_di_laurea")+"</a></p>");
+                } while (res.next());
+            } else {
+                out.print("Nessuno studente presente");
+            }
+        %>
+    </head>
+    <body>
+        <br>
+        <h3>Inserisci studente</h3>
+        <form action="/servlet" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome"><br>
+            <label for="cognome">Cognome:</label>
+            <input type="text" id="cognome" name="cognome"><br>
+            <label for="corso">Corso di laurea:</label>
+            <input type="text" id="corso" name="corso"><br><br>
+            <button type="submit">Inserisci studente</button>
+            <input type="hidden" name="action" value="create">
+        </form>
+
+    </body>
+</html>
+
+
