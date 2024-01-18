45d86440de4334880f04d219a51b051fc5c7ba2d
nameisalfio <alfio.spoto@outlook.it>
2024-01-18 17:19:09 +0100

    Laravel one-to-many

--- Itinere_3/Laravel/Teams/app/Http/Controllers/TeamController.php
+++ Itinere_3/Laravel/Teams/app/Http/Controllers/TeamController.php
@@ -3,61 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Team;
+use App\Models\Player;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return view('players.index', ["players" => Player::all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Team $team)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Team $team)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Team $team)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(Team $team)
     {
         //
